fd8b14c7c63eac41194c09bd9206ed0aa6d76942
Rechidesigns <alerechiordu@gmail.com>
2023-05-17 02:03:36 +0100

    install reqirements

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -198,6 +199,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+
+
 
 MEDIA_URL = "media/"
 
